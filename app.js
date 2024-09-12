9275e052455fb8300bed8d7d7127eac153551d0b
tabsheermk <creativemk12@gmail.com>
2024-09-12 22:17:04 +0530

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import sellerRouter from "./routes/sellers.js";
 import buyerRouter from "./routes/buyers.js";
+import cors from "cors";
 
 export const app = express();
 
@@ -12,7 +13,13 @@ config({ path: "./data/config.env" });
 // Using middlewares
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, //need to set this true to be able to send cookies and track them
+  })
+);
 
 
 app.get("/", (req, res) => {
@@ -23,3 +30,6 @@ app.get("/", (req, res) => {
 app.use("/api/v1/sellers", sellerRouter);
 app.use("/api/v1/buyers", buyerRouter);
 
+
+//using custom error middleware
+app.use(errorMiddleware);
